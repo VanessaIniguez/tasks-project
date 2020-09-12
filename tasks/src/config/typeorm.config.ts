c4d5b6521439623f4a5ace0b5cf01210d83e4469
Vanessa Iñiguez <vane@pop-os.localdomain>
2020-09-11 20:21:59 -0500

    Create a docker-compose file and configuration typeorm with data base

---
+++ tasks/src/config/typeorm.config.ts
@@ -0,0 +1,11 @@
+import {TypeOrmModuleOptions} from '@nestjs/typeorm';
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: 'postgres',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: true
+}
